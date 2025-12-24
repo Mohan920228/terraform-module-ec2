866c88e5af1a9cf1f3c92e37efc27518ae7e593c
Mohan920228 <100313825+Mohan920228@users.noreply.github.com>
2025-12-24 22:38:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "ec2" {
   ami = "ami-068c0051b15cdb816"
   instance_type = "t3a.nano"
-  vpc_security_group_ids = [ "sg-035f460350721e6f3" ]
   subnet_id = "subnet-0a54e06d228e37fc2"
 }
